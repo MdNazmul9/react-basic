a940dff42290736f2d29bdd01e0753b290fb3d18
MdNazmul9 <nazmul.cse.pust@gmail.com>
2022-09-07 18:00:12 +0600

    premitive and reference

---
+++ Resources/referencePremitive.js
@@ -0,0 +1,10 @@
+const number = 1; // premitive 
+const num2 = number; // reference
+console.log(num2);
+
+
+const Person = {
+    name : "nazmul" //premitive
+}
+  const secondPerson = Person; // Reference
+  console.log(secondPerson) 
