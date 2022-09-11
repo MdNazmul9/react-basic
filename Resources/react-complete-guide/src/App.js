1e73d9890caa8ca3de74be96fdaf58caef581089
MdNazmul9 <nazmul.cse.pust@gmail.com>
2022-09-11 18:13:14 +0600

    splitting component among multiple components

--- Resources/react-complete-guide/src/App.js
+++ Resources/react-complete-guide/src/App.js
@@ -3,7 +3,10 @@ import ExpenseItem from "./components/ExpenseItem";
 function App() {
   const expense = [
     {
-      id: "e1",title: "Car Insurance", amount: 294.96, date: new Date(2022, 9, 10),
+      id: "e1",
+      title: "Car Insurance",
+      amount: 294.96,
+      date: new Date(2022, 9, 10),
     },
     { id: "e2", title: "Pen", amount: 30.4, date: new Date(2022, 9, 10) },
     { id: "e3", title: "Mouse", amount: 450.87, date: new Date(2022, 9, 10) },
@@ -16,23 +19,25 @@ function App() {
       <ExpenseItem
         title={expense[0].title}
         amount={expense[0].amount}
+
         date={expense[0].date}
-      ></ExpenseItem>
+
+      />
       <ExpenseItem
         title={expense[1].title}
         amount={expense[1].amount}
         date={expense[1].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expense[2].title}
         amount={expense[2].amount}
         date={expense[2].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expense[3].title}
         amount={expense[3].amount}
         date={expense[3].date}
-      ></ExpenseItem>
+      />
     </div>
   );
 }
