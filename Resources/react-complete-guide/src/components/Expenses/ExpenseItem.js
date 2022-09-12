bb17aa0f5d506bf0c8b3046884170958ac5b86b4
MdNazmul9 <nazmul.cse.pust@gmail.com>
2022-09-12 18:21:05 +0600

    Component function extended

--- Resources/react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ Resources/react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -4,16 +4,17 @@ import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-
+  let title = props.title;
   const clickHandeler = () => {
-    console.log("clicked")
-  }
+    console.log("clicked");
+    console.log(title);
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
 
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandeler}> Change Title</button>
