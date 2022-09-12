170e23d844f0cac4ffc1299e73a72423eef5532f
MdNazmul9 <nazmul.cse.pust@gmail.com>
2022-09-12 16:06:47 +0600

    components

--- Resources/react-complete-guide/src/App.js
+++ Resources/react-complete-guide/src/App.js
@@ -1,7 +1,9 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expeses from "./components/Expeses";
 
-function App() {
-  const expense = [
+
+const App = () => {
+
+  const expenses = [
     {
       id: "e1",
       title: "Car Insurance",
@@ -14,30 +16,13 @@ function App() {
   ];
 
   return (
-    <div className="App">
-      <h1> Ehsan Marketing!</h1>
-      <ExpenseItem
-        title={expense[0].title}
-        amount={expense[0].amount}
-        date={expense[0].date}
-      />
-      <ExpenseItem
-        title={expense[1].title}
-        amount={expense[1].amount}
-        date={expense[1].date}
-      />
-      <ExpenseItem
-        title={expense[2].title}
-        amount={expense[2].amount}
-        date={expense[2].date}
-      />
-      <ExpenseItem
-        title={expense[3].title}
-        amount={expense[3].amount}
-        date={expense[3].date}
-      />
+    <div>
+    <h1> Ehsan Marketing!</h1>
+    <Expeses expenses={expenses}/>
     </div>
+      
+   
   );
-}
+};
 
 export default App;
