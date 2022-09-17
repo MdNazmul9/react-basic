57804cc1426ddee4db27fe634fbbb040f175e9cb
MdNazmul9 <nazmul.cse.pust@gmail.com>
2022-09-17 17:40:59 +0600

    outputting conditional content 1

--- Resources/react-complete-guide/src/components/Expenses/Expenses.js
+++ Resources/react-complete-guide/src/components/Expenses/Expenses.js
@@ -6,16 +6,29 @@ import ExpensesFilter from "./ExpensesFilter";
 import "../UI/Card.css";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2022');
+  const [filteredYear, setFilteredYear] = useState("2021");
 
   const filterChangeHandeler = (selectYear) => {
     setFilteredYear(selectYear);
   };
 
-  const filteredExpenses = props.items.filter(expense => {
+  const filteredExpenses = props.items.filter((expense) => {
     console.log(expense);
-    return expense.date.getFullYear().toString()=== filteredYear;
+    return expense.date.getFullYear().toString() === filteredYear;
   });
+
+  let expensesContent = <p>There is no content</p>;
+  console.log("filteredExpenses.length: ", filteredExpenses.length);
+  // if (filteredExpenses.length > 0){
+  //   expensesContent = filteredExpenses.map((expense) => (
+  //     <ExpenseItem
+  //       key = {expense.id}
+  //       title={expense.title}
+  //       amount={expense.amount}
+  //       date={expense.date}
+  //     />
+  //   ))
+  // }
 
   return (
     <Card className="expenses">
@@ -23,14 +36,20 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandeler}
       ></ExpensesFilter>
-      {filteredExpenses.map((expense) => (
-        <ExpenseItem
-          key = {expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
+
+
+      {filteredExpenses.length === 0? <p className="notFound"> No content found</p>:
+
+   
+      filteredExpenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))
+      }
     </Card>
   );
 };
