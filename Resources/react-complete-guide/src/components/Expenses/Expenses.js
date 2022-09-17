0281fb01ea9c59a6dfc3bf5818c649cf5af9e871
MdNazmul9 <nazmul.cse.pust@gmail.com>
2022-09-17 16:15:30 +0600

    Understanding _Keys

--- Resources/react-complete-guide/src/components/Expenses/Expenses.js
+++ Resources/react-complete-guide/src/components/Expenses/Expenses.js
@@ -5,22 +5,26 @@ import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
-
   const [filteredYear, setFilteredYear] = useState("2020");
 
- const filterChangeHandeler = (selectYear) => {
-  setFilteredYear(selectYear);
- }
+  const filterChangeHandeler = (selectYear) => {
+    setFilteredYear(selectYear);
+  };
 
   return (
     <Card className="expenses">
-      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandeler}></ExpensesFilter>
-      { props.items.map(expense => <ExpenseItem
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />)}
-      
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandeler}
+      ></ExpensesFilter>
+      {props.items.map((expense) => (
+        <ExpenseItem
+          key = {expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
     </Card>
   );
 };
