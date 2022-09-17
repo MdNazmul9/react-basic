cb8ce2d8f7f30c23ef285313ac1fafc48bd2e534
MdNazmul9 <nazmul.cse.pust@gmail.com>
2022-09-17 13:00:02 +0600

    stateless component

--- Resources/react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ Resources/react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -5,23 +5,22 @@ import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
 
-  const [title, setTitle] = useState(props.title);
-  console.log("Expense item evaluated by react");
+  // const [title, setTitle] = useState(props.title);
+  // console.log("Expense item evaluated by react");
 
-  const clickHandeler = () => {
-    // console.log("clicked");
-    setTitle("Updeated!")
-    console.log(title);
-  };
+  // const clickHandeler = () => {
+  //   // console.log("clicked");
+  //   setTitle("Updeated!")
+  //   console.log(title);
+  // }; 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
 
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandeler}> Change Title</button>
     </Card>
   );
 };
