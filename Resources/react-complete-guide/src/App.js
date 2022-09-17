da8ef76adbf2cd2fac1ec900b6914dac6bd16b01
MdNazmul9 <nazmul.cse.pust@gmail.com>
2022-09-17 16:01:24 +0600

    statefull List

--- Resources/react-complete-guide/src/App.js
+++ Resources/react-complete-guide/src/App.js
@@ -1,30 +1,31 @@
 import Expenses from "./components/Expenses/Expenses";
-import React from "react";
+import React, { useState } from "react";
 import NewExpense from "./components/NewExpense/NewExpense";
 
-const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Car Insurance",
-      amount: 294.96,
-      date: new Date(2022, 9, 15),
-    },
-    { id: "e2", title: "Pen", amount: 30.4, date: new Date(2022, 9, 11) },
-    { id: "e3", title: "Mouse", amount: 450.87, date: new Date(2022, 7, 10) },
-    { id: "e4", title: "Laptop", amount: 7200.95, date: new Date(2022, 5, 20) },
-  ];
+const DUMMY_DATA = [
+  {
+    id: "e1",
+    title: "Car Insurance",
+    amount: 294.96,
+    date: new Date(2022, 9, 15),
+  },
+  { id: "e2", title: "Pen", amount: 30.4, date: new Date(2022, 9, 11) },
+  { id: "e3", title: "Mouse", amount: 450.87, date: new Date(2020, 7, 10) },
+  { id: "e4", title: "Laptop", amount: 7200.95, date: new Date(2019, 5, 20) },
+];
 
-  // return React.createElement(
-  //   'div',
-  //   {},
-  //   React.createElement('h1', {}, "Ehsan's Marketing!"),
-  //   React.createElement(Expenses, {items: expenses}, ),
-  // );
+const App = () => {
+
+  const [expenses, setExpenses] = useState(DUMMY_DATA);
+  
 
   const addExpenseHandeler = (expense) => {
     console.log("In App.js");
     console.log(expense);
+    setExpenses((prevExpenses)=>{
+      return [expense, ...prevExpenses]
+    });
+
   };
 
   return (
