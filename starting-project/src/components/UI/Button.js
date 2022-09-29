5445b43b3adccb6f1ffd8178b97eae4cd9c901e8
MdNazmul9 <nazmul.cse.pust@gmail.com>
2022-09-29 13:27:55 +0600

    @media quiry

--- starting-project/src/components/UI/Button.js
+++ starting-project/src/components/UI/Button.js
@@ -5,22 +5,25 @@ import React from "react";
 import styled from "styled-components";
 
 const Button = styled.button`
-  .button {
-    font: inherit;
-    padding: 0.5rem 1.5rem;
-    border: 1px solid #8b005d;
-    color: white;
-    background: #8b005d;
-    box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-    cursor: pointer;
-  }
+  width: 100%;
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #8b005d;
+  color: white;
+  background: #8b005d;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
 
-  .button:focus {
+  @media (min-width: 768px) {
+    width: auto;
+  }
+  
+  &:focus {
     outline: none;
   }
 
-  .button:hover,
-  .button:active {
+  &:hover,
+  &:active {
     background: #8b005d;
     border-color: #8b005d;
     box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
