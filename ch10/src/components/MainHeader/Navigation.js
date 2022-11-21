22614077b478f90786b31836c87ad832349d1037
MdNazmul9 <nazmul.cse.pust@gmail.com>
2022-11-21 19:39:59 +0600

    useEffect

--- ch10/src/components/MainHeader/Navigation.js
+++ ch10/src/components/MainHeader/Navigation.js
@@ -1,22 +1,26 @@
-import React from "react";
+import React from 'react';
 
-import classes from "./Navigation.module.css";
+import classes from './Navigation.module.css';
 
 const Navigation = (props) => {
   return (
     <nav className={classes.nav}>
       <ul>
         {props.isLoggedIn && (
-            <li>
-                <a href="/">Admin</a>
-            </li>
+          <li>
+            <a href="/">Users</a>
+          </li>
         )}
         {props.isLoggedIn && (
-            <li>
-                <button onClick={props.onLogout}>Logout</button>
-            </li>
+          <li>
+            <a href="/">Admin</a>
+          </li>
         )}
-
+        {props.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+        )}
       </ul>
     </nav>
   );
